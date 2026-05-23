8482972cc7b5e588de877572e9abd16205622ab2
jorgitopp08-code <jorgitopp08@gmail.com>
2026-05-23 16:30:49 -0500

    Add files via upload

---
+++ _test_levels.py
@@ -0,0 +1,13 @@
+import importlib.util
+spec=importlib.util.spec_from_file_location('domino', r'c:\Users\jesa9\juego2026\.py')
+mod=importlib.util.module_from_spec(spec)
+spec.loader.exec_module(mod)
+for nivel in ['facil','normal','dificil']:
+    juego=mod.JuegoDomino('2v2',['Tú','IA1','IA2','IA3'], nivel=nivel)
+    print('nivel', nivel, '->', juego.nivel, 'turno inicial', juego.turno)
+    for p in [1,2,3]:
+        jugables=[str(f) for f in juego.fichas_jugables(p)]
+        print('  IA jugador', p, 'jugables', jugables)
+        res=juego.ia_jugar(p)
+        print('   jugó?', res)
+    print('---')
